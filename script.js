51a0356e0356beac96635fd9dc355e143b8f7d93
ghilman naqvi <ghilmanali51@gmail.com>
2025-02-21 18:19:09 +0500

    Portfolio landing page incomp

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-var tl = gsap.timeline()
+// var tl = gsap.timeline()
 
-gsap.from("li", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
-gsap.from("button", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
-tl.from("h2", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
-tl.from("h1", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
-tl.from("h6", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
-gsap.from("p", {
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    opacity: 0
-})
+// gsap.from("li", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// gsap.from("button", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// tl.from("h2", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// tl.from("h1", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// tl.from("h6", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// gsap.from("p", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
+// gsap.from(".img-box", {
+//     y: 20,
+//     duration: 1,
+//     delay: 0.2,
+//     opacity: 0
+// })
